3145daed3d08967680a2d47318c0066c5bacb67d
Moin Khan <moinkhan1119rja@gmail.com>
2026-04-01 02:37:16 +0100

    status section created || 1 April 2026 02:37 AM}

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { ToastContainer } from "react-toastify";
 import "./App.css";
 import NavBar from "./components/navBar/NavBar";
 import Banner from "./components/banner/Banner";
+import Status from "./components/status/Status";
 
 function App() {
   return (
     <>
       <NavBar></NavBar>
       <Banner></Banner>
+      <Status></Status>
 
       <ToastContainer></ToastContainer>
     </>
---
+++ src/components/status/Status.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Status = () => {
+  return (
+    <div className="bg-linear-to-r from-primary via-[#4F39F6] to-secondary">
+      <div className="mx-auto text-center text-white flex justify-between items-center w-10/12 py-10 mt-10">
+        <div>
+          <h2 className="text-5xl font-bold">50K+</h2>
+          <br />
+          <p className="text-2xl font-semibold">Active Users</p>
+        </div>
+        <div class="w-px h-24 bg-gray-400"></div>
+        <div>
+          <h2 className="text-5xl font-bold">200+</h2>
+          <br />
+          <p className="text-2xl font-semibold">Premium Tools</p>
+        </div>
+        <div class="w-px h-24 bg-gray-400"></div>
+        <div>
+          <h2 className="text-5xl font-bold">4.9</h2>
+          <br />
+          <p className="text-2xl font-semibold">Rating</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Status;
